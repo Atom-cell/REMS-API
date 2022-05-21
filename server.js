6d4eb0caaa25194a8ed2ed26c5c47b3f566ae920
Atom-cell <rajpootsaniusnain@gmail.com>
2022-05-22 02:11:35 +0500

    desktop routes aded

--- server.js
+++ server.js
@@ -7,8 +7,12 @@ const http = require("http").createServer(app);
 const PORT = 5000;
 const adminRouter = require("./routes/admin.route");
 const empRouter = require("./routes/emp.route");
+const desktopRouter = require("./routes/desktop.route");
 const io = require("socket.io")(http);
 let sock = {};
+
+app.use(express.json({ limit: "25mb" }));
+app.use(express.urlencoded({ limit: "25mb" }));
 
 const mongoose = require("mongoose");
 mongoose.connect("mongodb://localhost:27017/REMSruff", (err) => {
@@ -29,6 +33,7 @@ io.on("connection", (socket) => {
   //socket.emit("code", "2216");
 
   socket.on("Email", (data) => {
+    console.log("SOCKET EMAIL");
     sock[data] = socket.id;
     console.log(sock[data]);
   });
@@ -43,8 +48,10 @@ io.on("connection", (socket) => {
   });
 
   io.to(socket.id).emit("Ex", `Exclusive Message ${Math.random(100)}`);
+
   socket.on("disconnect", () => {
     console.log("disconnected", socket.id);
+    //delete socket[]
   });
 
   socket.on("fromCLIENT", (data) => {
@@ -52,8 +59,26 @@ io.on("connection", (socket) => {
   });
 });
 
+// from web visit this route, then on utility it will emit. front se email send. idher email se sock mai se id nikalo or emit krdo usko
+app.get("/start", (req, res) => {
+  let e = "naseer@gmail.com";
+  let a = sock[e];
+  console.log("Sending signal to turn on SS", a);
+  io.to(a).emit("SSStart", "true");
+  res.send("hello ");
+});
+
+app.get("/stop", (req, res) => {
+  let e = "naseer@gmail.com";
+  let a = sock[e];
+  console.log("Sending signal to turn on SS", a);
+  io.to(a).emit("SSStop", "true");
+  res.send("hello ");
+});
+
 app.use("/admin", adminRouter);
 app.use("/emp", empRouter);
+app.use("/desk", desktopRouter);
 
 http.listen(process.env.PORT || 5000, () => {
   console.log("Server online at ", PORT);
